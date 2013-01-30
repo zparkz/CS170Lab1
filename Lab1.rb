7d33260ca8450f0d8dd307dad2c1a528487cee38
Willow S <wsolem@gmail.com>
2013-01-29 20:34:21 -0800

    The first lab for CS170. Just put your answers after the comments.

---
+++ Lab1.rb
@@ -0,0 +1,30 @@
+# 1. Print the numbers from 1 to 10 to the console without writing a for loop or a while loop. Put each number on its own line.
+# I'll get  you started! 
+
+my_num = 1
+
+
+# 2. Get input from the console and save it to a variable. Prompt the user to enter a string such as their name or school. 
+#  	Print to the console the string in caps and reversed, then print out the length of the string. 
+
+
+
+=begin 
+3. Using the variable you created in question 2, print out its type. Now set that variable equal to a number, such as the length
+ of that string, and print out that type. What does this mean about Ruby variables? 
+=end
+
+
+
+# 4. Create a Ruby object with two fields, such as for a first and last name. 
+
+
+
+# 5. Now give this object two more fields. 
+
+
+# 6. Get input from the console, and if it is a string, assign it to a field in your object. 
+
+
+# 7. Create at least one more object of the same type, and create an array of these objects. Iterate through the array and 
+# print to the console one field, such as thre first name. 
